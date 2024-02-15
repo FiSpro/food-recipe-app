6b8e5c763e35ee66302bf90ba4504c4679e3a53e
Filip Sprostran <filipspro@gmail.com>
2024-02-15 19:29:28 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://api.edamam.com";
 
-export const APP_ID = "2e529169";
+export const APP_ID = // Enter your app id;
 
-export const APP_KEY = "a45b3d9bded37feecb4019334caa3582";
+export const APP_KEY = // Enter your app key;
